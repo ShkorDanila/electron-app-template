a8e7b7070a2dc4ec52c802370df700cfa3421951
Shkor Danila <shkor.danila@gmail.com>
2024-02-12 17:21:50 +0300

    Added Electron and Vite Projects

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # electron-app-template
 React-TypeScript Template for Electron
-| React-router-dom, Zustand, Next.js, Axios, Webpack included
+| React-router-dom, Zustand, Axios, Vite included
---
+++ react/src/App.tsx
@@ -0,0 +1,26 @@
+import './App.css'
+import { HashRouter, Routes, Route } from "react-router-dom";
+
+// Роутинг вписан, на всякий юзать NavLink
+// Если что-то непонятно по билдингу или самым базам,
+// ссылка на видос с гайдером по электрону
+// https://www.youtube.com/watch?v=-sfEusBvWRE&t=179s
+// ПЕРЕД БИЛДОМ ПОСМОТРЕТЬ
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello world</h1>
+      <HashRouter>
+        <Routes>
+          <Route>
+            
+          </Route>
+        </Routes>
+      </HashRouter>
+    </>
+  )
+}
+
+export default App
